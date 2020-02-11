603cd7e4c0a3950e412d95abe5a112e5a3d5846c
makram89 <34419130+makram89@users.noreply.github.com>
2020-02-11 01:48:51 +0100

    rs

--- water/views.py
+++ water/views.py
@@ -30,3 +30,12 @@ class PumpStateViewSet(viewsets.ReadOnlyModelViewSet):
         station_id = self.kwargs['station_id']
         pump_id = self.kwargs['pump_id']
         return PumpState.objects.filter(station_state__station_id=station_id, pump__pump_id_=pump_id).all()
+
+
+def receive_water_data(request, station_id):
+    if request.method == 'PUT':
+        try:
+            request_data = json.loads(request.body)
+
+        except:
+            pass

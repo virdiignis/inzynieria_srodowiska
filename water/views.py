35dd912b6729112e227a83fa34905fbcbd7e3532
virdiignis <gbryk1@gmail.com>
2020-02-11 01:39:26 +0100

    reeeeeturn

--- water/views.py
+++ water/views.py
@@ -10,7 +10,7 @@ class ValveStateViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         station_id = self.kwargs['station_id']
         valve_id = self.kwargs['valve_id']
-        ValveState.objects.filter(station_state__station_id=station_id, valve__valve_id=valve_id).all()
+        return ValveState.objects.filter(station_state__station_id=station_id, valve__valve_id=valve_id).all()
 
 
 class ContainerStateViewSet(viewsets.ReadOnlyModelViewSet):
@@ -19,7 +19,8 @@ class ContainerStateViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         station_id = self.kwargs['station_id']
         container_id = self.kwargs['container_id']
-        ContainerState.objects.filter(station_state__station_id=station_id, container__container_id=container_id).all()
+        return ContainerState.objects.filter(station_state__station_id=station_id,
+                                             container__container_id=container_id).all()
 
 
 class PumpStateViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,6 +29,4 @@ class PumpStateViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         station_id = self.kwargs['station_id']
         pump_id = self.kwargs['pump_id']
-        PumpState.objects.filter(station_state__station_id=station_id, pump__pump_id_=pump_id).all()
-
-
+        return PumpState.objects.filter(station_state__station_id=station_id, pump__pump_id_=pump_id).all()
